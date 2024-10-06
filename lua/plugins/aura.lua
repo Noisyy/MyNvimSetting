20dbc4cf77a088ac4fc935c79a7138b499306dbc
Noisyy <nguyennhut99vl@gmail.com>
2024-10-06 15:20:54 +0700

    Orgin

---
+++ lua/plugins/aura.lua
@@ -0,0 +1,21 @@
+return {
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  {
+    "baliestri/aura-theme",
+    lazy = false,
+    priority = 1000,
+    config = function(plugin)
+      vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+    end,
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    config = function()
+      require("nightfox").setup({
+        options = {
+          transparent = true,
+        },
+      })
+    end,
+  },
+}
